54fb6fb72314c12b38c6ca5a31fd1bc80652d79a
Jasmine <jsjohn026@gmail.com>
2019-04-04 16:41:20 -0700

    update project

--- building_active_record_lite/lib/01_sql_object.rb
+++ building_active_record_lite/lib/01_sql_object.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require_relative 'db_connection'
 require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
@@ -14,10 +15,9 @@ class SQLObject
   end
 
   def self.finalize!
-      self.columns.each do |col|
-        @attributes[col] = define_method(col) { self.instance_variable_get("@#{col}") }
-
-        @attributes["#{col}="] = define_method("#{col}=") { |val| self.instance_variable_set("@#{col}", val) }
+    self.columns.each do |col|
+      define_method(col) { self.attributes[col] }
+      define_method("#{col}=") { |val| self.attributes[col] = val} #Q: JJ would col += work instead of interpolation
     end
   end
 
@@ -31,19 +31,33 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+    SELECT * FROM #{self.table_name}
+    SQL
+
+    self.parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |h| self.new(h) } # .new is built-in and calls initialize
   end
 
   def self.find(id)
-    # ...
+    results = DBConnection.execute(<<-SQL, id)
+    SELECT * FROM #{self.table_name}
+    WHERE id = ? 
+    SQL
+
+    self.parse_all(results).first
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      raise "unknown attribute '#{attr_name}'" unless self.class.columns.include?(attr_name.to_sym) #Q: JJ self.class v. SQLObjects.class clarification
+      method_name = "#{attr_name}="
+      method = self.methods.select { |m| m.to_s == method_name }.first #Q: JJ
+      self.send(method,value) #Q: calling method is ok here b/c line 46 guarantees it is there. 
+    end
   end
 
   def attributes
@@ -51,11 +65,18 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |c| self.send(c) } #Q: JJ self.attributes?
   end
 
   def insert
-    # ...
+    col_names = self.class.columns.join(",")
+    question_marks = "(" + Array.new(self.class.columns.count, "?").join(",") + ")"
+    results = DBConnection.execute(<<-SQL, *attribute_values)
+    INSERT INTO #{self.class.table_name} (#{col_names})
+    VALUES #{question_marks}
+    SQL
+    foo = DBConnection.insert_id
+    puts foo
   end
 
   def update
